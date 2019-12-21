ac1495e501afbd59dd7abb04825869592d754768
Jason Manuel <jama.indo@hotmail.com>
2019-12-21 14:33:01 -0700

    Add a TodoCalendar class
    This class can display the number of todos due on a given day for each day in a month.

--- todo_txt_calendar.py
+++ todo_txt_calendar.py
@@ -1,7 +1,8 @@
 import sys
 import re
 import datetime
-from typing import Iterable
+import calendar
+from typing import Iterable, Optional, Callable
 
 
 class Todo:
@@ -27,6 +28,37 @@ class Todo:
         due_text = ' due ' + str(self.__due_date) if self.__due_date else ''
         return f'{self.__text}{due_text}'
 
+    @property
+    def due_date(self) -> Optional[datetime.date]:
+        return self.__due_date
+
+
+class TodoCalendar(calendar.TextCalendar):
+
+    def __init__(self, todos: Iterable[Todo], firstweekday: int = 0):
+        super().__init__(firstweekday)
+        self.__todos = todos
+
+    def __count_todos_for_date(self, date: datetime.date) -> int:
+        def predicate(todo: Todo) -> bool:
+            return bool(todo.due_date) and todo.due_date == date
+        return len(tuple(filter(predicate, self.__todos)))
+
+    def formatmonth(self, year: int, month: int, width: int = 0, height=0) -> str:
+        def get_abbr(n: int) -> str:
+            return calendar.day_abbr[n][:-1]
+
+        def make_week_string(week: Iterable[int]) -> str:
+            return (''.join(map(lambda d: d and str(f'{str(d).ljust(2)} ({self.__count_todos_for_date(datetime.date(year, month, d))})').ljust(
+                width) or ' ' * width, week)))
+
+        month_str = calendar.month_name[month]
+        min_width = 6
+        width = max(width, min_width)
+        weekday_abbrs = map(get_abbr, self.iterweekdays())
+        week_header = (' ' * (width - 2)).join(weekday_abbrs)
+        days = '\n'.join(make_week_string(week) for week in self.monthdayscalendar(year, month))
+        return f'{month_str} {year}\n{week_header}\n{days}\n'
 
 def parse_todos(string: str) -> Iterable[Todo]:
     for line in string.splitlines():
